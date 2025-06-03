b6b5c3b21f2ce406db03a19e80c1e4fa2eccc60f
cats <blackmeoth@gmail.com>
2025-06-03 10:16:22 +0800

    step 10 : index page done

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from listings.models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 # Create your views here.
 def index(request):
     listings = Listing.objects.all()
     paginator = Paginator(listings, 3)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
-    context = {"listings" : paged_listings}
+    context = {"listings" : paged_listings,
+    "price_choices" : price_choices,
+    "bedroom_choices" : bedroom_choices,
+    "district_choices" : district_choices}
     return render(request,'listings/listings.html', context)
 
 def listing(request,listing_id):
